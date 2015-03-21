48e99941d101ce090b5365d300abe99f884ffca9
Alexander Emelyanov <alexander.emelyanov@faunusanalytics.com>
2015-03-21 21:14:21 +0300

    User profile page added. Logout action implemented. Lesson #6 (http://habrahabr.ru/post/223375/) is ready

--- app/views.py
+++ app/views.py
@@ -12,17 +12,15 @@ def index():
     user = g.user
     posts = [  # список выдуманных постов
         {
-            'author': { 'nickname': 'John' },
+            'author': {'nickname': 'John'},
             'body': 'Beautiful day in Portland!'
         },
         {
-            'author': { 'nickname': 'Susan' },
+            'author': {'nickname': 'Susan'},
             'body': 'The Avengers movie was so cool!'
         }
     ]
     return render_template("index.html", title='Home', user=user, posts=posts)
-
-# функция представления index опущена для краткости
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -42,22 +40,54 @@ def after_login(resp):
     if resp.email is None or resp.email == "":
         flash('Invalid login. Please try again.')
         return redirect(url_for('login'))
+
     user = User.query.filter_by(email=resp.email).first()
+
     if user is None:
         nickname = resp.nickname
         if nickname is None or nickname == "":
             nickname = resp.email.split('@')[0]
+
         user = User(nickname=nickname, email=resp.email, role=ROLE_USER)
         db.session.add(user)
         db.session.commit()
+
     remember_me = False
+
     if 'remember_me' in session:
         remember_me = session['remember_me']
         session.pop('remember_me', None)
+
     login_user(user, remember=remember_me)
+
     return redirect(request.args.get('next') or url_for('index'))
 
 
 @app.before_request
 def before_request():
     g.user = current_user
+
+
+@app.route('/user/<nickname>')
+@login_required
+def user_profile(nickname):
+
+    user = User.query.filter_by(nickname=nickname).first()
+
+    if user is None:
+        flash('User ' + nickname + ' not found.')
+        return redirect(url_for('index'))
+
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+
+    return render_template('user.html', user=user, posts=posts)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))

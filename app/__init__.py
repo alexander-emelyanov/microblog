bc6f57fb7422e26d779ca1e587a32cd2104a0851
Alexander Emelyanov <alexander.emelyanov@faunusanalytics.com>
2015-03-22 23:52:15 +0300

    Error handling - base version. Send email to admin, when errors occurred.

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 
-from config import basedir
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD, MAIL_SECURE
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -23,3 +23,23 @@ def load_user(id):
     return models.User.query.get(int(id))
 
 from app import views
+
+
+# Error handling
+if not app.debug:
+
+    import logging
+    from logging.handlers import SMTPHandler
+
+    credentials = None
+    secure = None
+
+    if MAIL_USERNAME or MAIL_PASSWORD:
+        credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+
+    if MAIL_SECURE:
+        secure = MAIL_SECURE
+
+    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), MAIL_USERNAME, ADMINS, 'Microblog failure', credentials, secure)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
